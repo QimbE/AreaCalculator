1bd955c3a4947b101eb8e67c60e6e06d3ce590ad
QimbE <58182585+QimbE@users.noreply.github.com>
2023-11-17 21:42:08 +0500

    merge: develop to master
    commit eb37921aedbefc9203c4cb869ecbb20a64423a6d
    Author: QimbE <58182585+QimbE@users.noreply.github.com>
    Date:   Fri Nov 17 21:35:50 2023 +0500
    
        docs: add more comments
    
    commit ecec38389f0ada953303b6deeaaf2353244ba237
    Author: QimbE <58182585+QimbE@users.noreply.github.com>
    Date:   Fri Nov 17 21:12:06 2023 +0500
    
        feat: add demonstration
    
    commit e28bf10ccdfcb7db0dc6f6b21bd7965fc8026dda
    Author: QimbE <58182585+QimbE@users.noreply.github.com>
    Date:   Fri Nov 17 21:03:32 2023 +0500
    
        tests: triangle tests
    
    commit 335e4441c78157d1fba8ce6269fc3e293d5a11c5
    Author: QimbE <58182585+QimbE@users.noreply.github.com>
    Date:   Fri Nov 17 21:03:22 2023 +0500
    
        tests: circle tests
    
    commit c4c45c09668260ce014641bbf83fd1d52722ed2d
    Author: QimbE <58182585+QimbE@users.noreply.github.com>
    Date:   Fri Nov 17 21:03:09 2023 +0500
    
        chore: add tests project
    
    commit c7dbd92368c918ecdf2924d7f50642328f5a0804
    Author: QimbE <58182585+QimbE@users.noreply.github.com>
    Date:   Fri Nov 17 21:02:50 2023 +0500
    
        feat: add triangle
    
    commit 6336b900e6429e1dd8a86a054843c62f0289b5ff
    Author: QimbE <58182585+QimbE@users.noreply.github.com>
    Date:   Fri Nov 17 21:02:23 2023 +0500
    
        refactor: replace methods with virtuals
    
    commit c2cf527a49a5d4c4a1a6e22e8ee94ba9ca8faa9f
    Author: QimbE <58182585+QimbE@users.noreply.github.com>
    Date:   Fri Nov 17 19:37:36 2023 +0500
    
        feat: add circle shape
    
    commit e02e196969ae6d410705ff0473f7f8dd6ffe4f61
    Author: QimbE <58182585+QimbE@users.noreply.github.com>
    Date:   Fri Nov 17 19:37:12 2023 +0500
    
        feat: add shape abstractions
    
    commit d7c7e1ff0f58e9843771a32df884712bb0a93314
    Author: QimbE <58182585+QimbE@users.noreply.github.com>
    Date:   Fri Nov 17 19:36:51 2023 +0500
    
        chore: add library project

--- SurfaceCalc/ISurfaceCalculatable.cs
+++ SurfaceCalc/ISurfaceCalculatable.cs
@@ -1,6 +1,13 @@
 ﻿namespace SurfaceCalc;
 
+/// <summary>
+/// Contract to compute surface of something.
+/// </summary>
 public interface ISurfaceCalculatable
 {
+    /// <summary>
+    /// Calculates surface of instance.
+    /// </summary>
+    /// <returns>The surface value.</returns>
     double CalculateSurface();
 }
