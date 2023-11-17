c2cf527a49a5d4c4a1a6e22e8ee94ba9ca8faa9f
QimbE <58182585+QimbE@users.noreply.github.com>
2023-11-17 19:37:36 +0500

    feat: add circle shape

---
+++ SurfaceCalc/Circle.cs
@@ -0,0 +1,33 @@
+﻿namespace SurfaceCalc;
+
+public class Circle: Shape, ISurfaceCalculatable
+{
+    private double _radius;
+    
+    public double Radius
+    {
+        get => _radius;
+        set
+        {
+            if (value < 0)
+            {
+                // Probably should throw an exception
+                _radius = 0;
+            }
+            else
+            {
+                _radius = value;
+            }
+        }
+    }
+    
+    public Circle(double radius)
+    {
+        Radius = radius;
+    }
+    
+    public double CalculateSurface()
+    {
+        return Math.PI * Radius * Radius;
+    }
+}
